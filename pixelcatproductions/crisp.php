db8e56710b6fbe46224692f9f0c9f2edc3699357
JustinBack <jback@pixelcatproductions.net>
2020-12-26 13:32:44 +0100

    Change version to 0.0.8-beta.RC4

--- pixelcatproductions/crisp.php
+++ pixelcatproductions/crisp.php
@@ -27,7 +27,7 @@ namespace crisp;
 class core {
     /* Some important constants */
 
-    const CRISP_VERSION = "0.0.8-beta.RC3";
+    const CRISP_VERSION = "0.0.8-beta.RC4";
 
     /**
      * This is my autoloader. 

45e16db6fd933ee66ff0240598575cb5dcc8ce28
JustinBack <jback@pixelcatproductions.net>
2020-12-30 21:20:07 +0100

    Added additional links

--- bin/update_all_plugins.sh
+++ bin/update_all_plugins.sh
@@ -7,3 +7,11 @@ php cli.php plugin translations reinstall discourse
 php cli.php plugin translations reinstall heroku
 php cli.php plugin translations reinstall statuspage
 php cli.php theme translations reinstall crisp
+
+
+php cli.php plugin storage reinstall admin
+php cli.php plugin storage reinstall core
+php cli.php plugin storage reinstall discourse
+php cli.php plugin storage reinstall heroku
+php cli.php plugin storage reinstall statuspage
+php cli.php theme storage reinstall crisp
--- themes/crisp/hook.php
+++ themes/crisp/hook.php
@@ -5,4 +5,4 @@
 \crisp\core\Theme::addtoNavbar("api", crisp\api\Translation::fetch("navbar_item_api"), "/api", "_self", -95);
 \crisp\core\Theme::addtoNavbar("forum", crisp\api\Translation::fetch("navbar_item_forum"), \crisp\api\Config::get("forum_url"), "_self", -94);
 \crisp\core\Theme::addtoNavbar("presskit", crisp\api\Translation::fetch("presskit"), "/presskit", "_self", -93);
-
+\crisp\core\Theme::addtoNavbar("donate", crisp\api\Translation::fetch("donate"), \crisp\api\Config::get("opencollective_url"), "_self", 0, "right");

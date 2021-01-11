958618f90caedcc1cde3ef04da5e100af8626be8
JustinBack <jback@pixelcatproductions.net>
2021-01-11 18:58:40 +0100

    Changes to Migration

--- pixelcatproductions/class/crisp/migrations/0_createmigration.php
+++ pixelcatproductions/class/crisp/migrations/0_createmigration.php
@@ -4,19 +4,20 @@ namespace crisp\migrations;
 
 class createmigration extends \crisp\core\Migrations {
 
-    public function run() {
-        try {
-            $this->begin();
-            \crisp\core\Migrations::createTable("schema_migration", array("file", \crisp\core\Migrations::DB_VARCHAR));
+  public function run() {
+    try {
+      $this->begin();
+      \crisp\core\Migrations::createTable("schema_migration", array("file", \crisp\core\Migrations::DB_VARCHAR));
 
-            $this->addColumn("schema_migration", array("plugin", self::DB_VARCHAR, "DEFAULT NULL"));
-
-            return $this->end();
-        } catch (\Exception $ex) {
-            echo $ex->getMessage() . PHP_EOL;
-            $this->rollback();
-            return false;
-        }
+      if (!$this->isMigrated("1608994339_addplugintomigration")) {
+        $this->addColumn("schema_migration", array("plugin", self::DB_VARCHAR, "DEFAULT NULL"));
+      }
+      return $this->end();
+    } catch (\Exception $ex) {
+      echo $ex->getMessage() . PHP_EOL;
+      $this->rollback();
+      return false;
     }
+  }
 
 }
--- pixelcatproductions/class/crisp/migrations/7_installtheme.php
+++ pixelcatproductions/class/crisp/migrations/7_installtheme.php
@@ -4,18 +4,18 @@ namespace crisp\migrations;
 
 class installtheme extends \crisp\core\Migrations {
 
-    public function run() {
-        try {
-            $this->begin();
-            $this->Database->query("INSERT INTO Config (`key`, value) VALUES ('theme_dir', 'themes')")->execute();
-            $this->Database->query("INSERT INTO Config (`key`, value) VALUES ('plugin_dir', 'plugins')")->execute();
-            \crisp\core\Themes::install("crisp");
-            return $this->end();
-        } catch (\Exception $ex) {
-            echo $ex->getMessage() . PHP_EOL;
-            $this->rollback();
-            return false;
-        }
+  public function run() {
+    try {
+      $this->begin();
+      $this->Database->query("INSERT INTO Config (`key`, value) VALUES ('theme_dir', 'themes')");
+      $this->Database->query("INSERT INTO Config (`key`, value) VALUES ('plugin_dir', 'plugins')");
+      \crisp\core\Themes::install("crisp");
+      return $this->end();
+    } catch (\Exception $ex) {
+      echo $ex->getMessage() . PHP_EOL;
+      $this->rollback();
+      return false;
     }
+  }
 
 }

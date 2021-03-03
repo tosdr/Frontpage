cb217659e5d209dd0097d040c1b32ad4d51e9ada
Justin René Back <jback@pixelcatproductions.net>
2021-03-03 10:33:06 +0100

    Add INSTALL_PLUGINS

--- pixelcatproductions/class/crisp/core/Bitmask.php
+++ pixelcatproductions/class/crisp/core/Bitmask.php
@@ -47,6 +47,7 @@ class Bitmask {
     public const SUPERUSER = 0x00002000;
     public const MERGE_CASE = 0x00004000;
     public const CHANGE_KV = 0x00008000;
+    public const INSTALL_PLUGINS = 0x00010000;
 
     public static function hasBitmask(int $BitwisePermissions, int $PermissionFlag = 0x00000000) {
         if (!is_numeric($BitwisePermissions)) {

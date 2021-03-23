72aaa2a9ce311ed2c3cb417e45ab63889c16c7b8
Justin René Back <jback@pixelcatproductions.net>
2021-03-23 13:10:34 +0100

    Add grid embeds

---
+++ themes/crisp/includes/embed.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!isset($GLOBALS["route"]->GET["q"])) {
+    header("Location: /");
+    exit;
+}
+try {
+    if (is_numeric($GLOBALS["route"]->GET["q"])) {
+        $_vars = array("service" => \crisp\api\Phoenix::getServicePG($GLOBALS["route"]->GET["q"]));
+    } else {
+        $_vars = array("service" => \crisp\api\Phoenix::getServiceByNamePG(urldecode($GLOBALS["route"]->GET["q"])));
+    }
+} catch (\Exception $ex) {
+    header("Location: /");
+    exit;
+}

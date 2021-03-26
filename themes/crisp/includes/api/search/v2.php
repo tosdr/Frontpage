de1d68837dd4d595aece28d0cdfe49ccb554b448
Justin René Back <jback@pixelcatproductions.net>
2021-03-26 21:42:43 +0100

    Fix bug in search/v2

--- themes/crisp/includes/api/search/v2.php
+++ themes/crisp/includes/api/search/v2.php
@@ -1,14 +1,18 @@
 <?php
 
 if (empty($this->Query) || !isset($this->Query)) {
-    foreach (\crisp\api\Config::get("frontpage_services") as $ID) {
-        $Array[] = crisp\api\Phoenix::getServicePG($ID);
-    }
-} else {
-    foreach (crisp\api\Phoenix::searchServiceByNamePG(strtolower($this->Query)) as $Service) {
-        $Array[] = $Service;
-    }
+    echo \crisp\core\PluginAPI::response(crisp\core\Bitmask::QUERY_FAILED, "Empty request", (array(
+        "results" => 0,
+        "service" => []
+    )));
+    return;
 }
+
+
+foreach (crisp\api\Phoenix::searchServiceByNamePG(strtolower($this->Query)) as $Service) {
+    $Array[] = $Service;
+}
+
 if (count($Array) > 0) {
     echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::REQUEST_SUCCESS, $this->Query, (array(
         "results" => count($Array),

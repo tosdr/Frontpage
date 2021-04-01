f2190c4d3d26917c8fa419008481b64ee014e3bc
Justin René Back <jback@pixelcatproductions.net>
2021-04-01 13:25:37 +0200

    Send correct service id

--- plugins/curator/includes/views/service_requests.php
+++ plugins/curator/includes/views/service_requests.php
@@ -33,20 +33,21 @@ if (isset($_POST["approve"]) && !empty($_POST["approve"])) {
     $Domains = $request["domains"];
     $Wikipedia = $request["wikipedia"];
     $Documents = json_decode($request["documents"], true);
-
+    $service_id;
     $newstatement = $Phoenix->getDBConnector()->prepare("INSERT INTO services (name, url, wikipedia, created_at, updated_at) VALUES (:name, :url, :wikipedia, NOW(), NOW())");
 
     if ($newstatement->execute([":name" => $Name, ":url" => $Domains, ":wikipedia" => $Wikipedia])) {
+        $service_id = $Phoenix->getDBConnector()->lastInsertId();
         foreach ($Documents as $Document) {
             $newstatementdoc = $Phoenix->getDBConnector()->prepare("INSERT INTO documents (name, url, xpath, created_at, updated_at, service_id) VALUES (:name, :url, :xpath, NOW(), NOW(), :service_id)");
-            $newstatementdoc->execute([":name" => $Document["name"], ":url" => $Document["url"], ":xpath" => $Document["xpath"], ":service_id" => $Phoenix->getDBConnector()->lastInsertId()]);
+            $newstatementdoc->execute([":name" => $Document["name"], ":url" => $Document["url"], ":xpath" => $Document["xpath"], ":service_id" => $service_id]);
         }
 
         $request = $Mysql->getDBConnector()->prepare("DELETE FROM service_requests WHERE id = :id;");
         $request->execute([":id" => $_POST["approve"]]);
 
 
-        echo \crisp\core\PluginAPI::response(crisp\core\Bitmask::REQUEST_SUCCESS, $Phoenix->getDBConnector()->lastInsertId(), []);
+        echo \crisp\core\PluginAPI::response(crisp\core\Bitmask::REQUEST_SUCCESS, $service_id, []);
         exit;
     } else {
         echo \crisp\core\PluginAPI::response(crisp\core\Bitmask::GENERIC_ERROR, "SQL Error" . var_export($newstatement->errorInfo(), true), []);

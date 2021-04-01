8424b2eeef4e70c85dde8bb95c66537870a8bab8
Justin René Back <jback@pixelcatproductions.net>
2021-04-01 12:40:51 +0200

    Add new service to navbar

--- themes/crisp/hook.php
+++ themes/crisp/hook.php
@@ -9,6 +9,7 @@
 \crisp\core\Theme::addtoNavbar("donate", crisp\api\Translation::fetch("components.navbar.donate"), \crisp\api\Helper::generateLink(\crisp\api\Config::get("opencollective_url"), true), "_self", 0, "right");
 \crisp\core\Theme::addtoNavbar("about", crisp\api\Translation::fetch("components.navbar.about"), \crisp\api\Helper::generateLink("about"), "_self", -97);
 \crisp\core\Theme::addtoNavbar("downloads", crisp\api\Translation::fetch("components.navbar.download"), \crisp\api\Helper::generateLink("downloads"), "_self", -96);
+\crisp\core\Theme::addtoNavbar("new_service", crisp\api\Translation::fetch("components.navbar.request_service"), \crisp\api\Helper::generateLink("request_service"), "_self", -96);
 
 if (\crisp\api\Config::get("maintenance_enabled") || isset($_GET["simulate_maintenance"])) {
   http_response_code(503);

17fcde8158801c5f72076bc30953bb1dc53b953f
Justin René Back <jback@pixelcatproductions.net>
2021-04-06 03:28:51 +0200

    Remove bash files

--- bin/update_all_plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-php cli.php plugin refresh admin
-php cli.php plugin refresh core
-php cli.php plugin refresh discourse
-php cli.php plugin refresh heroku
-php cli.php plugin refresh statuspage
-php cli.php theme refresh crisp

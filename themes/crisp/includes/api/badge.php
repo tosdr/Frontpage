6451adae2377cbc16a11283adcbb35e083db6f62
Justin René Back <jback@pixelcatproductions.net>
2021-04-09 16:15:33 +0200

    Fix Badge generation by ID

--- themes/crisp/includes/api/badge.php
+++ themes/crisp/includes/api/badge.php
@@ -48,7 +48,7 @@ if (!is_numeric($ServiceName)) {
         echo $poser->generate($Prefix, $Rating, $Color, 'flat');
         return;
     }
-    $RedisData = \crisp\api\Phoenix::getServiceBySlugPG(urldecode($ServiceName));
+    $RedisData["_source"] = \crisp\api\Phoenix::getServiceBySlugPG(urldecode($ServiceName));
 } else {
     if (count(crisp\api\Phoenix::serviceExistsPG($ServiceName)) === 0) {
         header("Content-Type: image/svg+xml");
@@ -88,10 +88,7 @@ switch ($RedisData["is_comprehensively_reviewed"] ? ($RedisData["rating"]) : fal
         $Rating = $Translations->fetch("badges.grade.none");
 }
 
-header("X-API-Service: " . $RedisData["id"]);
-header("X-API-Service-Rating: " . $RedisData["rating"]);
-header("X-API-Service-Reviewed: " . $RedisData["is_comprehensively_reviewed"]);
-$Prefix = \crisp\api\Config::get("badge_prefix") . "/#" . htmlentities($RedisData["slug"]);
+$Prefix = \crisp\api\Config::get("badge_prefix") . "/#" . htmlentities($RedisData["_source"]["slug"]);
 
 $SVG = $poser->generate($Prefix, $Rating, $Color, 'flat');
 
@@ -99,28 +96,28 @@ if (!file_exists(__DIR__ . "/../../../../pixelcatproductions/cache/badges/")) {
     mkdir(__DIR__ . "/../../../../pixelcatproductions/cache/badges/");
 }
 
-if (time() - filemtime(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".svg") > 900) {
-    file_put_contents(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".svg", $SVG);
+if (time() - filemtime(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".svg") > 900) {
+    file_put_contents(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".svg", $SVG);
 }
 
 if ($GLOBALS["route"]->Page === "badgepng" || $Type == "png") {
     header("Content-Type: image/png");
 
-    if (!file_exists(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".svg")) {
+    if (!file_exists(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".svg")) {
         echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::GENERATE_FAILED, "FS Source SVG not found", [], null, 500);
         exit;
     }
 
-    if (time() - filemtime(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".png") > 900) {
+    if (time() - filemtime(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".png") > 900) {
 
-        exec("/usr/bin/inkscape -e \"" . __DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".png\" \"" . __DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".svg\"");
+        exec("/usr/bin/inkscape -e \"" . __DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".png\" \"" . __DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".svg\"");
 
-        if (!file_exists(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".png")) {
+        if (!file_exists(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".png")) {
             echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::GENERATE_FAILED, "FS PNG not found", [], null, 500);
             exit;
         }
     }
-    echo file_get_contents(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["id"] . $Language) . ".png");
+    echo file_get_contents(__DIR__ . "/../../../../pixelcatproductions/cache/badges/" . sha1($Prefix . $RedisData["_source"]["id"] . $Language) . ".png");
     exit;
 }
 

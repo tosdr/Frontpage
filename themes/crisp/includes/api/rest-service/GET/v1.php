e43a5a467381d60daf9b7531fdb88822fa23a812
Justin René Back <jback@pixelcatproductions.net>
2021-04-11 02:48:59 +0200

    Deprecate rest-service/v1

--- themes/crisp/includes/api/rest-service/GET/v1.php
+++ themes/crisp/includes/api/rest-service/GET/v1.php
@@ -20,19 +20,19 @@
 
 if (!is_numeric($_GET["service"] ?? $this->Query)) {
     if (!crisp\api\Phoenix::serviceExistsBySlugPG($_GET["service"] ?? $this->Query)) {
-        echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::INVALID_SERVICE, $_GET["service"] ?? $this->Query, []);
+        echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::INVALID_SERVICE + \crisp\core\Bitmask::VERSION_DEPRECATED, $_GET["service"] ?? $this->Query, []);
         return;
     }
     $_GET["service"] ?? $this->Query = crisp\api\Phoenix::getServiceBySlugPG($_GET["service"] ?? $this->Query)["id"];
-    echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::REQUEST_SUCCESS, $_GET["service"] ?? $this->Query, \crisp\api\Phoenix::generateApiFiles($_GET["service"] ?? $this->Query, "3"));
+    echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::REQUEST_SUCCESS + \crisp\core\Bitmask::VERSION_DEPRECATED, $_GET["service"] ?? $this->Query, \crisp\api\Phoenix::generateApiFiles($_GET["service"] ?? $this->Query, "3"));
     exit;
 }
 
 if (!crisp\api\Phoenix::serviceExistsPG($_GET["service"] ?? $this->Query)) {
-    echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::INVALID_SERVICE, $_GET["service"] ?? $this->Query, []);
+    echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::INVALID_SERVICE + \crisp\core\Bitmask::VERSION_DEPRECATED, $_GET["service"] ?? $this->Query, []);
     return;
 }
 
 
 
-echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::REQUEST_SUCCESS, $_GET["service"] ?? $this->Query, \crisp\api\Phoenix::generateApiFiles($_GET["service"] ?? $this->Query, "3"));
+echo \crisp\core\PluginAPI::response(\crisp\core\Bitmask::REQUEST_SUCCESS + \crisp\core\Bitmask::VERSION_DEPRECATED, $_GET["service"] ?? $this->Query, \crisp\api\Phoenix::generateApiFiles($_GET["service"] ?? $this->Query, "3"));

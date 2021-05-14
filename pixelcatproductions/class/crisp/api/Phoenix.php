af4672836e3dab0e5bbb66fab114fa85f6f906b5
Justin René Back <justin@tosdr.org>
2021-05-14 18:00:26 +0200

    Remove getCases API-8

--- pixelcatproductions/class/crisp/api/Phoenix.php
+++ pixelcatproductions/class/crisp/api/Phoenix.php
@@ -556,52 +556,6 @@ class Phoenix {
     }
 
     /**
-     * Get a list of cases
-     * @param bool $Force Force update from phoenix
-     * @return object
-     * @deprecated Use Phoenix::getCasesPG
-     * @throws Exception
-     */
-    public static function getCases(bool $Force = false) {
-        if (self::$Redis_Database_Connection === null) {
-            self::initDB();
-        }
-
-        if (self::$Redis_Database_Connection->exists(Config::get("phoenix_api_endpoint") . "/cases") && !$Force) {
-            return json_decode(self::$Redis_Database_Connection->get(Config::get("phoenix_api_endpoint") . "/cases"));
-        }
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => Config::get("phoenix_url") . Config::get("phoenix_api_endpoint") . "/cases",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_USERAGENT => "CrispCMS ToS;DR",
-        ));
-        $raw = curl_exec($curl);
-        $response = json_decode($raw);
-
-        if ($response === null) {
-            throw new Exception("Failed to crawl! " . $raw);
-        }
-
-        if ($response->error) {
-            throw new Exception($response->error);
-        }
-
-
-        if (self::$Redis_Database_Connection->set(Config::get("phoenix_api_endpoint") . "/cases", json_encode($response), 3600)) {
-            return $response;
-        }
-        throw new Exception("Failed to contact REDIS");
-    }
-
-    /**
      * List all services from postgres
      * @see https://github.com/tosdr/edit.tosdr.org/blob/8b900bf8879b8ed3a4a2a6bbabbeafa7d2ab540c/db/schema.rb#L134-L148 Database Schema
      * @return array

b5c9d6eac0ee4548d13523a0821f5f22a2d75208
Justin René Back <justin@tosdr.org>
2021-05-16 17:14:38 +0200

    Add RefreshToken GrantType

--- pixelcatproductions/class/crisp/core/OAuth.php
+++ pixelcatproductions/class/crisp/core/OAuth.php
@@ -35,7 +35,10 @@ class OAuth
 
         $server = new OAuth2\Server($storage);
 
+
+
         $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+        $server->addGrantType(new OAuth2\GrantType\RefreshToken($storage));
 
         return $server;
     }

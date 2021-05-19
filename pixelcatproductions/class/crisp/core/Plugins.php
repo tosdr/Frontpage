035a39fe1efc3494698da2c19537ab2a2844c30f
Justin René Back <justin@tosdr.org>
2021-05-19 13:24:49 +0200

    Plugins::registerInstallHook(): Add type hint, update docblock

--- pixelcatproductions/class/crisp/core/Plugins.php
+++ pixelcatproductions/class/crisp/core/Plugins.php
@@ -840,10 +840,10 @@ class Plugins
     /**
      * Registers an install hook for your plugin.
      * @param string $PluginName
-     * @param string|function $Function Callback function, either anonymous or a string to a function
-     * @returns boolean TRUE if hook could be registered, otherwise false
+     * @param mixed $Function Callback function, either anonymous or a string to a function
+     * @return bool
      */
-    public static function registerInstallHook($PluginName, $Function)
+    public static function registerInstallHook(string $PluginName, mixed $Function): bool
     {
         if (is_callable($Function) || function_exists($PluginName)($Function)) {
             self::on("pluginInstall_$PluginName", $Function);

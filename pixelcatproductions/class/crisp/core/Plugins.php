8d49061bbb8c20e311838895d73c419661f9f50b
Justin René Back <justin@tosdr.org>
2021-05-19 13:20:26 +0200

    Plugins::uninstallTranslations(): Add type hint, update docblock

--- pixelcatproductions/class/crisp/core/Plugins.php
+++ pixelcatproductions/class/crisp/core/Plugins.php
@@ -568,10 +568,10 @@ class Plugins
     /**
      * Uninstall all translations by plugin
      * @param string $PluginName The name of the plugin
-     * @param string $PluginMetadata plugin.json contents decoded
+     * @param stdClass $PluginMetadata plugin.json contents decoded
      * @return bool
      */
-    public static function uninstallTranslations($PluginName, $PluginMetadata)
+    public static function uninstallTranslations(string $PluginName, stdClass $PluginMetadata): bool
     {
         if (!is_object($PluginMetadata) && !isset($PluginMetadata->hookFile)) {
             return false;

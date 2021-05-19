2ce147c3c6ab3980a226c5c60c55e3e0ac85dbe5
Justin René Back <justin@tosdr.org>
2021-05-19 11:34:03 +0200

    Helper::currentURL(): Add type hint, update docblock

--- pixelcatproductions/class/crisp/api/Helper.php
+++ pixelcatproductions/class/crisp/api/Helper.php
@@ -382,7 +382,10 @@ class Helper
     }
 
 
-    public static function currentURL()
+    /**
+     * @return string
+     */
+    public static function currentURL(): string
     {
         return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }

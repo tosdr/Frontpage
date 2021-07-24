dad3a630f091cd5ce49e5c6c7ad5cd3352bf8cfb
Justin René Back <justin.back@tosdr.org>
2021-07-24 16:23:07 +0200

    Fix https://sentry.tosdr.org/share/issue/bfd98145853d4157b87123238e30f82f/
    Signed-off-by: Justin René Back <justin.back@tosdr.org>

--- themes/crisp/includes/_prod/frontpage.php
+++ themes/crisp/includes/_prod/frontpage.php
@@ -26,7 +26,6 @@ if(!defined('CRISP_COMPONENT')){
 }
 
 $Services = [];
-$EnvFile = parse_ini_file(__DIR__ . '/../../../.env');
 
 if (!isset($_GET['search'])) {
     foreach (Config::get('frontpage_services') as $ID) {

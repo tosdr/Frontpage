25cc76a89bf590bc35e02447144c5a3565e46051
Justin René Back <j.back@jrbit.de>
2023-11-02 15:53:30 +0000

    Major Rewrite WIP

---
+++ public/ThemeHook.php
@@ -0,0 +1,45 @@
+<?php
+
+use crisp\api\Helper;
+use crisp\models\HookFile;
+use \Twig\Environment;
+use crisp\core\Router;
+use crisp\core\Themes;
+use crisp\types\RouteType;
+use tosdr\PageControllers\FrontpagePageController;
+use tosdr\Phoenix;
+use \Twig\TwigFunction;
+
+class ThemeHook
+{
+
+    public function preExecute(): void
+    {
+    }
+    public function postExecute(): void
+    {
+    }
+
+    public function postRender(): void
+    {
+    }
+
+    public function preRender(): void
+    {
+    }
+    public function setup(): void
+    {
+
+        Themes::getRenderer()->addFunction(new TwigFunction('getService', [Phoenix::class, 'getService']));
+        Themes::getRenderer()->addFunction(new TwigFunction('getPoint', [Phoenix::class, 'getPoint']));
+        Themes::getRenderer()->addFunction(new TwigFunction('getPointsByService', [Phoenix::class, 'getPointsByService']));
+        Themes::getRenderer()->addFunction(new TwigFunction('getPointsByServiceScored', [Phoenix::class, 'getPointsByServiceScored']));
+
+
+        Helper::prettyDump($GLOBALS);
+
+
+        # Public Routes
+        Router::add("/", RouteType::PUBLIC, FrontpagePageController::class);
+    }
+}
